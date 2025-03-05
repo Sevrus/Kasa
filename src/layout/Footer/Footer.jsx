0a97cc911d27e30fe819545f12c10623be915f7c
Sevrus <book-n-troll@nordnet.fr>
2025-03-05 16:58:53 +0100

    feat: completed footer component implementation
    - Finalized the footer component with responsive design
    - Added copyright information and links
    - Styled the footer for consistency across devices

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -1,6 +1,14 @@
+import logo from "../../assets/images/logo_footer.svg";
+import styles from './Footer.module.scss';
+
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer className={styles.footer}>
+            <div className={styles.footer__logo}>
+                <img src={logo} alt="Le logo du site"/>
+            </div>
+            <p className={styles.footer__copyright}>© 2020 Kasa. All<span>rights reserved</span></p>
+        </footer>
     );
 }
 
