a5746dbfd608d471ee832e5044b4223f3ad19cce
Sevrus <book-n-troll@nordnet.fr>
2025-03-06 15:23:28 +0100

    feat: complete Banner component with overlay, text styling, and responsive design

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,9 +1,14 @@
 import styles from './Banner.module.scss';
 
-const Banner = ({image, alt}) => {
+const Banner = ({image, alt, text}) => {
     return (
         <div className={styles.banner}>
-            <img src={image} alt={alt}/>
+            <img className={styles.banner__image} src={image} alt={alt}/>
+            <div className={styles.banner__overlay}></div>
+            <p
+                className={styles.banner__text}
+                dangerouslySetInnerHTML={{__html: text}}
+            ></p>
         </div>
     );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,11 +2,13 @@ import Banner from "../../components/Banner/Banner.jsx";
 import image from "../../assets/images/home_banner.jpg";
 
 const Home = () => {
+    const textOverlay = `Chez vous, <span>partout et ailleurs</span>`;
     return (
         <>
             <Banner
                 image={image}
                 alt="Une côte escarpée plongeant dans une mer calme."
+                text={textOverlay}
             />
         </>
     );
