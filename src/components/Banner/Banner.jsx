9a4194470cd65ad9580dccddefdd01a8ea6b953f
Sevrus <book-n-troll@nordnet.fr>
2025-03-10 11:34:10 +0100

    feat(Banner): add optional props and improve conditional rendering
    - Introduced new optional props  (default: true) and  (default: true) to the  component.
    - Improved conditional rendering to avoid unnecessary markup when  is empty or / is false.

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,14 +1,16 @@
 import styles from './Banner.module.scss';
 
-const Banner = ({image, alt, text}) => {
+const Banner = ({image, alt, text= "", showText = true, showOverlay = true}) => {
     return (
         <div className={styles.banner}>
             <img className={styles.banner__image} src={image} alt={alt}/>
-            <div className={styles.banner__overlay}></div>
-            <p
-                className={styles.banner__text}
-                dangerouslySetInnerHTML={{__html: text}}
-            ></p>
+            {showOverlay && <div className={styles.banner__overlay}></div>}
+            {showText && text && (
+                <p
+                    className={styles.banner__text}
+                    dangerouslySetInnerHTML={{__html: text}}
+                ></p>
+            )}
         </div>
     );
 }
